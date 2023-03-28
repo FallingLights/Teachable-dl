2259215e4ede68db39bce6abf42939aed86d5f35
FallingLights <48025618+FallingLights@users.noreply.github.com>
2023-03-28 14:21:51 +0200

    Added proper logging and a new CLI argument

--- main.py
+++ main.py
@@ -48,6 +48,7 @@ def remove_emojis(data):
 
 
 def clean_string(data):
+    logging.debug("Cleaning string: " + data)
     data = data.encode('ascii', 'ignore').decode('ascii')
     return remove_emojis(data).replace("\n", "-").replace(" ", "-").replace(":", "-") \
         .replace("/", "-").replace("|", "")
@@ -64,20 +65,22 @@ class TeachableDownloader:
         self.verbose = False
 
     def run(self, course_url, email, password):
+        logging.info("Starting download of course: " + course_url)
         try:
             self.login(course_url, email, password)
         except Exception as e:
-            print("Could not login: " + str(e))
+            logging.error("Could not login: " + str(e), exc_info=self.verbose)
             return
 
         try:
             self.pick_course_downloader(course_url)
         except Exception as e:
-            print("Could not download course: " + course_url + " cause: " + str(e))
+            logging.error("Could not download course: " + course_url + " cause: " + str(e))
 
         self.clean_up()
 
     def login(self, course_url, email, password):
+        logging.info("Logging in")
         self.driver.get(course_url)
         self.driver.implicitly_wait(20)
 
@@ -99,12 +102,13 @@ class TeachableDownloader:
         commit_element.click()
 
         self.driver.implicitly_wait(10)
+        logging.info("Logged in, switching to course page")
         self.driver.get(course_url)
 
     def pick_course_downloader(self, course_url):
         self.driver.get(course_url)
         self.driver.implicitly_wait(10)
-
+        logging.info("Picking course downloader")
         if self.driver.find_elements(By.ID, "__next"):
             logging.info('Choosing __next format')
             self.download_course_next(course_url)
@@ -118,22 +122,29 @@ class TeachableDownloader:
             logging.error("Downloader does not support this course template. Please open an issue on github.")
 
     def download_course_block(self, course_url):
-        print("Detected block course format")
+        logging.info("Detected block course format")
         try:
+            logging.info("Getting course title")
             course_title = WebDriverWait(self.driver, 10).until(
                 EC.presence_of_element_located((By.CSS_SELECTOR, ".course__title"))
             ).text
         except Exception as e:
+            logging.warning("Could not get course title, using tab title instead")
             course_title = self.driver.title
 
         course_title = clean_string(course_title)
         course_path = create_folder(course_title)
 
-        output_file = os.path.join(course_path, "course.html")
-        with open(output_file, 'w+') as f:
-            f.write(self.driver.page_source)
+        logging.info("Saving course html")
+        try:
+            output_file = os.path.join(course_path, "course.html")
+            with open(output_file, 'w+') as f:
+                f.write(self.driver.page_source)
+        except Exception as e:
+            logging.error("Could not save course html: " + str(e), exc_info=self.verbose)
 
         # Unhide all elements
+        logging.info("Unhiding all elements")
         self.driver.execute_script('[...document.querySelectorAll(".hidden")].map(e=>e.classList.remove("hidden"))')
 
         chapter_idx = 1
@@ -146,6 +157,7 @@ class TeachableDownloader:
             chapter_title = section.find_element(By.CSS_SELECTOR, ".block__curriculum__section__title").text
             chapter_title = clean_string(chapter_title)
             chapter_title = chapter_title = "{:02d}-{}".format(chapter_idx, chapter_title)
+            logging.info("Found chapter: " + chapter_title)
 
             download_path = os.path.join(course_path, chapter_title)
             os.makedirs(download_path, exist_ok=True)
@@ -161,6 +173,7 @@ class TeachableDownloader:
                                                           ".block__curriculum__section__list__item__lecture-name").text
                 lecture_title = clean_string(lecture_title)
                 lecture_title = ''.join(char for char in lecture_title if char in string.printable)
+                logging.info("Found lecture: " + lecture_title)
 
                 video_entity = {"link": lecture_link, "title": lecture_title, "idx": idx,
                                 "download_path": download_path}
@@ -170,21 +183,26 @@ class TeachableDownloader:
         self.download_videos_from_links(video_list)
 
     def download_course_mainbar(self, course_url):
-        print("Detected _mainbar course format")
+        logging.info("Detected _mainbar course format")
         course_title = WebDriverWait(self.driver, 10).until(
             EC.presence_of_element_located((By.CSS_SELECTOR, "body > section > div.course-sidebar > h2"))
         ).text
         course_title = clean_string(course_title)
+        logging.info("Found course title: " + course_title)
         course_path = create_folder(course_title)
 
-        output_file = os.path.join(course_path, "course.html")
-        with open(output_file, 'w+') as f:
-            f.write(self.driver.page_source)
+        try:
+            output_file = os.path.join(course_path, "course.html")
+            with open(output_file, 'w+') as f:
+                f.write(self.driver.page_source)
+        except Exception as e:
+            logging.error("Could not save course html: " + str(e), exc_info=self.verbose)
 
         # Get course image
         image_element = self.driver.find_elements(By.CLASS_NAME, "course-image")
 
         if image_element:
+            logging.info("Found course image")
             image_link = image_element[0].get_attribute("src")
             image_link_hd = re.sub(r"/resize=.+?/", "/", image_link)
             # try to download the image using the modified link first
@@ -194,7 +212,7 @@ class TeachableDownloader:
                 image_path = os.path.join(course_path, "course-image.jpg")
                 with open(image_path, "wb") as f:
                     f.write(response.content)
-                print("Image downloaded successfully.")
+                logging.info("Image downloaded successfully.")
             else:
                 # try to download the image using the original link
                 response = requests.get(image_link)
@@ -203,10 +221,12 @@ class TeachableDownloader:
                     image_path = os.path.join(course_path, "course-image.jpg")
                     with open(image_path, "wb") as f:
                         f.write(response.content)
-                    print("Image downloaded successfully.")
+                    logging.info("Image downloaded successfully.")
                 else:
                     # print a message indicating that the image download failed
-                    print("Failed to download image.")
+                    logging.warning("Failed to download image.")
+        else:
+            logging.warning("Could not find course image")
 
         chapter_idx = 1
         video_list = []
@@ -217,6 +237,7 @@ class TeachableDownloader:
             chapter_title = section.find_element(By.CSS_SELECTOR, ".section-title").text
             chapter_title = clean_string(chapter_title)
             chapter_title = chapter_title = "{:02d}-{}".format(chapter_idx, chapter_title)
+            logging.info("Found chapter: " + chapter_title)
 
             download_path = os.path.join(course_path, chapter_title)
             os.makedirs(download_path, exist_ok=True)
@@ -230,6 +251,7 @@ class TeachableDownloader:
 
                 lecture_title = section_item.find_element(By.CLASS_NAME, "lecture-name").text
                 lecture_title = clean_string(lecture_title)
+                logging.info("Found lecture: " + lecture_title)
 
                 video_entity = {"link": lecture_link, "title": lecture_title, "idx": idx,
                                 "download_path": download_path}
@@ -250,21 +272,22 @@ class TeachableDownloader:
         return course_title
 
     def download_course_next(self, course_url):
-        print("Detected next course format")
+        logging.info("Detected next course format")
         course_title = self.get_course_title_next(course_url)
+        logging.info("Found course title: " + course_title)
         course_path = create_folder(course_title)
-
-        # Download course image
-        image_element = self.driver.find_element(By.XPATH, "//*[@id=\"__next\"]/div/div/div[2]/div/div[1]/img")
 
         output_file = os.path.join(course_path, "course.html")
         try:
             with open(output_file, 'w+', encoding='utf-8') as f:
                 f.write(self.driver.page_source)
         except Exception as e:
-            print("Failed to save course html:" + str(e))
+            logging.error("Could not save course html: " + str(e), exc_info=self.verbose)
 
+        # Download course image
+        image_element = self.driver.find_element(By.XPATH, "//*[@id=\"__next\"]/div/div/div[2]/div/div[1]/img")
         if image_element:
+            logging.info("Found course image")
             image_link = image_element.get_attribute("src")
             # Save image
             image_path = os.path.join(course_path, "course-image.jpg")
@@ -275,12 +298,12 @@ class TeachableDownloader:
                 with open(image_path, "wb", encoding='utf-8') as f:
                     f.write(response.content)
                 # print a message indicating that the image was downloaded
-                print("Image downloaded successfully.")
+                logging.info("Image downloaded successfully.")
             except Exception as e:
                 # print a message indicating that the image download failed
-                print("Failed to download image:" + str(e))
+                logging.info("Failed to download image:" + str(e))
         else:
-            print("Image not found.")
+            logging.warning("Image not found.")
 
         chapter_idx = 0
         video_list = []
@@ -291,11 +314,11 @@ class TeachableDownloader:
             chapter_title = slim_section.find_element(By.CSS_SELECTOR, ".heading").text
             chapter_title = clean_string(chapter_title)
             chapter_title = "{:02d}-{}".format(chapter_idx, chapter_title)
-            print(chapter_title)
+            logging.info("Found chapter: " + chapter_title)
 
             not_available_element = slim_section.find_elements(By.CSS_SELECTOR, ".drip-tag")
-            if not_available_element: # Not the best way to check if the chapter is available
-                print("Chapter not available yet. Skipping...")
+            if not_available_element:  # Not the best way to check if the chapter is available
+                logging.warning('Chapter "%s" not available, skipping', chapter_title)
                 continue
 
             download_path = os.path.join(course_path, chapter_title)
@@ -308,6 +331,7 @@ class TeachableDownloader:
                 title = video.text
                 # Remove new line characters from the title and replace spaces with -
                 title = clean_string(title)
+                logging.info("Found lecture: " + title)
                 video_entity = {"link": link, "title": title, "idx": idx, "download_path": download_path}
                 video_list.append(video_entity)
                 idx += 1
@@ -318,16 +342,16 @@ class TeachableDownloader:
         for video in video_list:
             self.driver.get(video["link"])
             self.driver.implicitly_wait(10)
-
+            logging.info("Downloading video: " + video["title"])
             try:
                 self.save_webpage_as_html(video["title"], video["idx"], video["download_path"])
             except Exception as e:
-                print("Could not save html: " + video["title"] + " cause: " + str(e))
+                logging.error("Could not save html: " + video["title"] + " cause: " + str(e), exc_info=self.verbose)
 
             try:
                 self.download_attachments(video["link"], video["title"], video["idx"], video["download_path"])
             except Exception as e:
-                print("Could not download attachments: " + video["title"] + " cause: " + str(e))
+                logging.warning("Could not download attachments: " + video["title"] + " cause: " + str(e))
 
             try:
                 WebDriverWait(self.driver, timeout=10).until(
@@ -335,7 +359,7 @@ class TeachableDownloader:
                         (By.XPATH, "//iframe[starts-with(@data-testid, 'embed-player')]"))
                 )
             except Exception as e:
-                print("Could not switch to iframe: " + video["title"])
+                logging.warning("Could not find video: " + video["title"] + " cause: " + str(e))
                 continue
 
             script_text = self.driver.find_element(By.ID, "__NEXT_DATA__")
@@ -345,15 +369,15 @@ class TeachableDownloader:
             try:
                 self.download_subtitle(link, video["title"], video["idx"], video["download_path"])
             except Exception as e:
-                print("Could not download subtitle: " + video["title"] + " cause: " + str(e))
+                logging.warning("Could not download subtitle: " + video["title"] + " cause: " + str(e))
 
             try:
                 self.download_video(link, video["title"], video["idx"], video["download_path"])
             except Exception as e:
-                print("Could not download video: " + video["title"] + " cause: " + str(e))
+                logging.warning("Could not download video: " + video["title"] + " cause: " + str(e))
                 continue
 
-            print("Downloaded: " + video["title"])
+            logging.info("Downloaded: " + video["title"])
         return
 
     def download_video(self, link, title, video_index, output_path):
@@ -378,7 +402,7 @@ class TeachableDownloader:
             with yt_dlp.YoutubeDL(ydl_opts) as ydl:
                 ydl.download([link])
         except Exception as e:
-            print("Could not download video: " + title + " cause: " + str(e))
+            logging.error("Could not download video: " + title + " cause: " + str(e))
 
     # This function is needed because yt-dlp subtitle downloader is not working
     def download_subtitle(self, link, title, video_index, output_path):
@@ -408,7 +432,7 @@ class TeachableDownloader:
                 info = ydl.extract_info(link, download=False)
                 info_json = ydl.sanitize_info(info)
         except Exception as e:
-            print("Could not download subtitle: " + title + " cause: " + str(e))
+            logging.warning("Could not download subtitle: " + title + " cause: " + str(e))
 
         subtitle_links = {}
         for lang, sub_info in info_json["requested_subtitles"].items():
@@ -421,7 +445,7 @@ class TeachableDownloader:
             try:
                 req = requests.get(sub["url"], headers=self.headers)
             except Exception as e:
-                print("Could not download subtitle: " + title + " cause: " + str(e))
+                logging.warning("Could not download subtitle: " + title + " cause: " + str(e))
             relative_path = req.text.split("\n")[5]
             full_url = urljoin(base_url, relative_path)
             subtitle_filename = "{:02d}-{}.{}.{}".format(video_index, title, lang, sub["ext"])
@@ -431,7 +455,8 @@ class TeachableDownloader:
                 with open(file_path, "wb") as f:
                     f.write(response.content)
             except Exception as e:
-                print("Could not download subtitle: " + title + " cause: " + str(e))
+                logging.warning("Could not download subtitle: " + title + " cause: " + str(e))
+            logging.info("Downloaded subtitle: " + subtitle_filename)
 
     def download_attachments(self, link, title, video_index, output_path):
         video_title = "{:02d}-{}".format(video_index, title)
@@ -451,22 +476,25 @@ class TeachableDownloader:
                 for video_link in video_links:
                     link = video_link.get_attribute("href")
                     file_name = video_link.text
-                    print(link, file_name)
+                    logging.info("Downloading attachment: " + file_name + " for video: " + title)
                     # Download file and save the file in output_path directory
                     wget.download(link, out=output_path)
         else:
-            print("No attachments found for video: " + title)
+            logging.warning("No attachments found for video: " + title)
 
     def save_webpage_as_html(self, title, video_index, output_path):
         output_file = os.path.join(output_path, "{:02d}-{}.html".format(video_index, title))
         with open(output_file, 'w+', encoding='utf-8') as f:
             f.write(self.driver.page_source)
+        logging.info("Saved webpage as html: " + output_file)
 
     def save_webpage_as_pdf(self, title, video_index, output_path):
         output_file_pdf = os.path.join(output_path, "{:02d}-{}.pdf".format(video_index, title))
         self.driver.save_print_page(output_file_pdf)
+        logging.info("Saved webpage as pdf: " + output_file_pdf)
 
     def clean_up(self):
+        logging.info("Cleaning up")
         self.driver.quit()
 
 
@@ -475,17 +503,28 @@ if __name__ == "__main__":
     parser.add_argument("--url", required=True, help='URL of the course')
     parser.add_argument("--email", required=True, help='Email of the account')
     parser.add_argument("--password", required=True, help='Password of the account')
+    parser.add_argument('-v', '--verbose', action='count', default=0,
+                        help='Increase verbosity level (repeat for more verbosity)')
     # parser.add_argument('-o', '--output', help='Output directory for the downloaded subtitle', default='.')
     args = parser.parse_args()
+
+    if args.verbose == 0:
+        log_level = logging.WARNING
+    elif args.verbose == 1:
+        log_level = logging.INFO
+    else:
+        log_level = logging.DEBUG
+
+    logging.basicConfig(level=log_level, format='%(levelname)s: %(message)s')
 
     downloader = TeachableDownloader()
     try:
         downloader.run(args.url, args.email, args.password)
     except KeyboardInterrupt:
-        print("Interrupted by user")
+        logging.error("Interrupted by user")
         downloader.clean_up()
         sys.exit(1)
     except Exception as e:
-        print("Error: " + str(e))
+        logging.error("Error: " + str(e))
         downloader.clean_up()
         sys.exit(1)
